ca5f989996eab6a47f120b5c94f93c890d5ddf9b
brunny-eth <blulinski17@gmail.com>
2025-05-08 14:57:24 -0400

    fixed ts scraperapi problem

--- api/types/scraperapi-sdk.d.ts
+++ api/types/scraperapi-sdk.d.ts
@@ -1,1 +1,11 @@
-declare module 'scraperapi-sdk'; 
+declare module 'scraperapi-sdk' {
+  interface ScraperAPIClient {
+    get: (url: string, options?: any) => Promise<any>; // You might want to refine 'any' to a more specific type if you know the response structure
+    // Add other methods if you use them, e.g.:
+    // post: (url: string, data: any, options?: any) => Promise<any>;
+    // put: (url: string, data: any, options?: any) => Promise<any>;
+  }
+
+  function scraperapiClient(apiKey: string): ScraperAPIClient;
+  export default scraperapiClient;
+} 
