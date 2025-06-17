51a5e84decb67fa514fb5447fe40a28cce55c993
brunny-eth <blulinski17@gmail.com>
2025-06-17 16:58:35 -0400

    some backend refactor and clean up of codebase

--- api/utils/preprocessText.ts
+++
@@ -1,6 +0,0 @@
-export function preprocessRawRecipeText(text: string): string {
-  let processedText = text.trim();
-  processedText = processedText.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
-  processedText = processedText.replace(/\n{3,}/g, '\n\n');
-  return processedText;
-} 
